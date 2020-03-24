6335dff456b985990a9c881542d422c7bea36590
kevin <kevin.albajrami@gmail.com>
2020-03-24 19:35:40 +0300

    Added Basket Model

--- ShopApp/ShopApp.DataAccess.SQL/DataContext.cs
+++ ShopApp/ShopApp.DataAccess.SQL/DataContext.cs
@@ -17,5 +17,8 @@ namespace ShopApp.DataAccess.SQL
 
         public DbSet<Product> products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+    
     }
 }
