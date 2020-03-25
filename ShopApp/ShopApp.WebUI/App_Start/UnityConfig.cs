1ba49a79b41a591ce4bda2f0bc62699acb80487b
kevin <kevin.albajrami@gmail.com>
2020-03-25 05:32:39 +0300

    Create UI for Basket

--- ShopApp/ShopApp.WebUI/App_Start/UnityConfig.cs
+++ ShopApp/ShopApp.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using ShopApp.Core.Contracts;
 using ShopApp.Core.Models;
 using ShopApp.DataAccess.InMemory;
 using ShopApp.DataAccess.SQL;
+using ShopApp.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace ShopApp.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
