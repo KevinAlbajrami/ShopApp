2f687ae99603967abbca2c82a99c6ce495b49598
kevin <kevin.albajrami@gmail.com>
2020-03-25 04:13:43 +0300

    Basket Service added

---
+++ ShopApp/ShopApp.Core/ViewModel/BasketSummaryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Core.ViewModel
+{
+    public class BasketSummaryViewModel
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketValue { get; set; }
+
+        public BasketSummaryViewModel(){}
+        public BasketSummaryViewModel(int BasketCount, decimal BasketValue) {
+            this.BasketCount = BasketCount;
+            this.BasketValue = BasketValue;
+        }
+    }
+}
